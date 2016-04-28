033b91f01c4984c93963748571396244f3636ebe
Sergio Alvarez-Napagao <tranchis@gmail.com>
2016-04-28 14:55:16 +0100

    Initial commit

---
+++ src/gluon/core.clj
@@ -0,0 +1,6 @@
+(ns gluon.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/gluon/core_test.clj
@@ -0,0 +1,7 @@
+(ns gluon.core-test
+  (:require [clojure.test :refer :all]
+            [gluon.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
